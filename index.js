d24585d523a27ca803aafbe4ecfcb78e8bbfad70
Jason Climo <jfclimo@gmail.com>
2024-11-12 16:50:39 +1300

    Changed rand to be .15 or 15%

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function randomClick() {
                     timeout: 60000
                 });
 
-                const shouldClickContainer = Math.random() < 0.05;
+                const shouldClickContainer = Math.random() < 0.15;
 
                 if (shouldClickContainer) {
                     console.log('Rolling for container click... Success!');
